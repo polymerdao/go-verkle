7d0265d5606e6dbb18392a774be69eae1600b343
Supragya Raj <supragyaraj@gmail.com>
2022-03-04 00:10:03 +0530

    driver code

--- benchs/main.go
+++ benchs/main.go
@@ -11,19 +11,20 @@ import (
 )
 
 func main() {
-	benchmarkInsertInExisting()
+	// benchmarkInsertInExisting()
+	tempWay()
 }
 
 func benchmarkInsertInExisting() {
-	f, _ := os.Create("cpu.prof")
-	g, _ := os.Create("mem.prof")
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
-	defer pprof.WriteHeapProfile(g)
+	// f, _ := os.Create("cpu.prof")
+	// g, _ := os.Create("mem.prof")
+	// pprof.StartCPUProfile(f)
+	// defer pprof.StopCPUProfile()
+	// defer pprof.WriteHeapProfile(g)
 	// Number of existing leaves in tree
-	n := 1000000
+	n := 1000
 	// Leaves to be inserted afterwards
-	toInsert := 10000
+	toInsert := 100
 	total := n + toInsert
 
 	keys := make([][]byte, n)
@@ -51,6 +52,7 @@ func benchmarkInsertInExisting() {
 					panic(err)
 				}
 			}
+			fmt.Println("Computing root commitment")
 			root.ComputeCommitment()
 
 			// Now insert the 10k leaves and measure time
@@ -65,4 +67,47 @@ func benchmarkInsertInExisting() {
 			fmt.Printf("Took %v to insert and commit %d leaves\n", elapsed, toInsert)
 		}
 	}
+}
+
+func tempWay() {
+	f, _ := os.Create("cpu.prof")
+	g, _ := os.Create("mem.prof")
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+	defer pprof.WriteHeapProfile(g)
+	fmt.Println("Temp way begin ----")
+
+	startTime := time.Now()
+	root := verkle.New()
+	fmt.Println("create root took ", time.Now().Sub(startTime))
+
+	startTime = time.Now()
+	cmt := root.ComputeCommitment()
+	fmt.Println("empty root commitment gen took: ", time.Now().Sub(startTime), " com: ", cmt)
+
+	// for _, toInsert := range []int{1, 50, 100, 10000} {
+	// 	toInsertKeys := make([][]byte, toInsert)
+	// 	value := []byte("value")
+	// 	for i := 0; i < toInsert; i++ {
+	// 		key := make([]byte, 32)
+	// 		rand.Read(key)
+	// 		toInsertKeys[i] = key
+	// 	}
+
+	// 	// fmt.Println("toInsertKeys: ", toInsertKeys)
+
+	// 	start := time.Now()
+	// 	for _, k := range toInsertKeys {
+	// 		if err := root.Insert(k, value, nil); err != nil {
+	// 			fmt.Println("error ", err, " k ", k, " value ", value)
+	// 			panic(err)
+	// 		}
+	// 	}
+	// 	// fmt.Println("root ", root)
+	// 	root.ComputeCommitment()
+	// 	elapsed := time.Since(start)
+	// 	fmt.Printf("Took %v to insert and commit %d leaves\n", elapsed, toInsert)
+	// }
+
+	fmt.Println("Temp way end ----")
 }
